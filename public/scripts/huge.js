1b64c0cd384603eb707553dd1fd720dd204b21b6
Gannon Curran <gannoncurran@mac.com>
2014-12-19 09:11:59 -0800

    implements ajax query to menudata endpoint

--- public/scripts/huge.js
+++ public/scripts/huge.js
@@ -11,3 +11,49 @@
 // 	document.getElementById("body").classList.toggle("modal-active")
 // }
 // </script>
+
+
+var Huge = {}
+
+Huge.Comm = (function() {
+
+	var retrieveMenuData = function() {
+		var data 
+
+		request = new XMLHttpRequest();
+		request.open('GET', '/api/nav.json', true)
+
+		request.onload = function() {
+		  if (this.status >= 200 && this.status < 400){
+		    // Success!
+		    data = JSON.parse(this.response)
+		    console.log("response from server, parsed: ", data)
+		    return data
+		  } else {
+		    // We reached our target server, but it returned an error
+			console.log("We reached our target server, but it returned an error")
+			data = []
+		  }
+		}
+
+		request.onerror = function() {
+		  // There was a connection error of some sort
+			console.log("There was a connection error of some sort")
+			data = []
+		}
+
+		request.send()
+	}
+
+	return {
+		getMenuData: function() {
+			retrieveMenuData()
+		}
+	}
+
+})()
+
+
+window.onload = function() {
+	Huge.Comm.getMenuData()
+}
