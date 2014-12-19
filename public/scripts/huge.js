d765623d999aea0f69f5fe4533ad5f7479348e95
Gannon Curran <gannoncurran@mac.com>
2014-12-19 08:15:57 -0800

    initial js file

---
+++ public/scripts/huge.js
@@ -0,0 +1,13 @@
+// <!-- TODO: PULL INLINE SCRIPT OUT INTO APP JS -->
+// <script>
+// function menuOpen() {
+//     document.getElementById("body").classList.add("menu-open");
+// }
+// function menuClose() {
+//     document.getElementById("body").classList.remove("menu-open");
+// }
+// function menuSubToggle(el) {
+// 	el.classList.toggle("open")
+// 	document.getElementById("body").classList.toggle("modal-active")
+// }
+// </script>
